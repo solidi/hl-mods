1624915f3570166549cc269ac11c2fca1ce3b4ec
Doug Arcuri <xsurrealx@hotmail.com>
2020-12-23 18:29:02 -0500

    Add a build script to the workspace

---
+++ workspace/start-clean.bat
@@ -0,0 +1,13 @@
+SET msdev="C:\Program Files\Microsoft Visual Studio\Common\MSDev98\Bin\msdev"
+SET icedir=C:\Sierra\Half-Life\iceg
+SET hlexe=C:\Sierra\Half-Life\hl.exe
+
+REM https://docs.microsoft.com/en-us/previous-versions/visualstudio/visual-studio-6.0/aa699274(v=vs.60)
+%msdev% z:\src\dlls\hl.dsw /make "hl - Win32 Debug"
+
+ECHO Y|RMDIR %icedir% /Q /S
+MKDIR %icedir%
+ECHO Y|XCOPY Z:\redist "%icedir%" /E
+
+REM https://developer.valvesoftware.com/wiki/Command_Line_Options
+%hlexe% -dev -console -game iceg +sv_lan 1 +map stalkyard
