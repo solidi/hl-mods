4fd8ab7b7abb1f2fbb95c5dfbdbe5ea2cb8218aa
Doug Arcuri <xsurrealx@hotmail.com>
2020-12-26 09:51:38 -0500

    Harden the start script with errors

--- workspace/start-clean.bat
+++ workspace/start-clean.bat
@@ -5,6 +5,11 @@ SET hlexe=C:\Sierra\Half-Life\hl.exe
 REM https://docs.microsoft.com/en-us/previous-versions/visualstudio/visual-studio-6.0/aa699274(v=vs.60)
 %msdev% z:\src\dlls\hl.dsp /make "hl - Win32 Debug"
 
+IF %ERRORLEVEL% NEQ 0 (
+  ECHO Could not compile dll.
+  CMD /K
+)
+
 ECHO Y|RMDIR %icedir% /Q /S
 MKDIR %icedir%
 ECHO Y|XCOPY Z:\redist "%icedir%" /E
@@ -12,9 +17,18 @@ ECHO Y|XCOPY Z:\redist "%icedir%" /E
 REM Making PAK file
 COPY Z:\bin\qpakman.exe %icedir%
 CD %icedir%
-%icedir%\qpakman models -o pak0.pak
+%icedir%\qpakman models -o pak0.pak | FIND "FAILURE" >nul2>nul
+
+if %ERRORLEVEL% EQU 0 (
+  ECHO Could not recreate pak file.
+  CMD /K
+)
+
 ECHO Y|RMDIR %icedir%\models /Q /S
 DEL %icedir%\qpakman.exe
 
 REM https://developer.valvesoftware.com/wiki/Command_Line_Options
-%hlexe% -dev -console -game iceg +sv_lan 1 +map stalkyard
+%hlexe% -dev -windowed -console -game iceg +sv_lan 1 +map stalkyard
+
+REM Prevent command closing
+CMD /K
