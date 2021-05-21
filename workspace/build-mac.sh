b80c495ba1bfb36a019f7151201de688b42399fa
Doug Arcuri <xsurrealx@hotmail.com>
2021-05-20 21:15:55 -0400

    Repair build-mac.sh lib copy

--- workspace/build-mac.sh
+++ workspace/build-mac.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 gameLib=ice.dylib
+clientLib=client.dylib
 cfg=release
 while getopts c: flag
 do
@@ -25,6 +26,8 @@ make clean CFG=$cfg
 echo "Compile macOS ${gameLib}..."
 # https://unix.stackexchange.com/questions/256120/how-can-i-suppress-output-only-if-the-command-succeeds
 output=`make hl CFG=$cfg 2>&1` || (echo $output && false)
+mv $cfg/${gameLib} ../../libs/dlls
 
-echo 'Compile macOS client.dylib...'
+echo "Compile macOS ${clientLib}..."
 output=`make hl_cdll CFG=$cfg 2>&1` || (echo $output && false)
+mv $cfg/${clientLib} ../../libs/cl_dlls
