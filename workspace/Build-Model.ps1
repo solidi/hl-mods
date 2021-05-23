2e1038296631dfed5954875019156d55d7838277
Doug Arcuri <xsurrealx@hotmail.com>
2021-05-22 21:00:52 -0400

    First attempt at submodules -> compile tool -> build model

---
+++ workspace/Build-Model.ps1
@@ -0,0 +1,22 @@
+#Requires -Version 7.1
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+Set-PSDebug -Trace 0
+
+$host.UI.RawUI.WindowTitle = "Packaging Cold Ice GoldSrc"
+function Set-ConsoleColor ($bc, $fc) {
+    $Host.UI.RawUI.BackgroundColor = $bc
+    $Host.UI.RawUI.ForegroundColor = $fc
+    Clear-Host
+}
+Set-ConsoleColor 'DarkCyan' 'White'
+
+Import-Module $PSScriptRoot\powershell\Compile-Model.psm1 -Force -DisableNameChecking
+. ("$PSScriptRoot\$configFile.ps1")
+
+$rootDir = ${PSScriptRoot}.Trimend('\')
+$redistDir = "${rootDir}\redist"
+$modelsdir = "${rootDir}\models"
+$binDir = "${rootDir}\bin"
+
+Compile-Model $binDir "w_satchel" $modelsdir $redistDir\models
