2fc33336e8a52a09c9850ed63e4c31894a16f413
Doug Arcuri <xsurrealx@hotmail.com>
2021-06-12 18:29:40 -0400

    Remove local configuration files

--- workspace/Config.Docker.ps1
+++
@@ -1,5 +0,0 @@
-$Config = @{ 
-    'binDir'  = "C:\bin"
-    'msBuild' = "C:\BuildTools\MSBuild\Current\Bin\MSBuild"
-    'defintions' = 'VEST;SILENCER;CLUSTER_GRENADES;CROWBAR;KNIFE;RPG;TRIPMINE;CHUMTOAD;SNIPER_RIFLE;BOLT_GUN;RAILGUN;CANNON;DECOY'
-}
--- workspace/Config.Local.ps1
+++
@@ -1,5 +0,0 @@
-$Config = @{ 
-    'binDir' = 'z:\workspace\bin'
-    'msBuild' = 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild'
-    'defintions' = 'VEST;SILENCER;CLUSTER_GRENADES;CROWBAR;KNIFE;RPG;TRIPMINE;CHUMTOAD;SNIPER_RIFLE;BOLT_GUN;RAILGUN;CANNON;DECOY'
-}
