ca1d85b441a499fb17c524d9e2ce26987339378d
Doug Arcuri <xsurrealx@hotmail.com>
2021-12-13 14:59:20 -0500

    Update linux game dir and game params

--- workspace/start-ubuntu.sh
+++ workspace/start-ubuntu.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-icefolder=ice
-maxplayers=8
+icefolder=ice_beta1
+maxplayers=16
 hiDef=1
 while getopts "mh:" flag
 do
@@ -32,6 +32,6 @@ cp -a redist_hd/. /root/.steam/debian-installation/steamapps/common/Half-Life/${
 # chmod 775 -R ${icefolder}_hd/
 
 # export LD_LIBRARY_PATH=.
-# /home/surreal/.steam/debian-installation/steamapps/common/Half-Life/hl_linux -steam -console -dev -condebug -game ${icefolder} -windowed -gl -w 640 -h 480 +developer 2 +log on +sv_lan 1 +map stalkyard +deathmatch 1 +maxplayers 8 -windowed
+# /home/surreal/.steam/debian-installation/steamapps/common/Half-Life/hl_linux -steam -console -dev -condebug -game ${icefolder} -windowed -gl -w 640 -h 480 +developer 2 +log on +sv_lan 1 +map snowyard +deathmatch 1 +maxplayers 8 -windowed
 
-/usr/games/steam steam://rungameid/70//-console%20-dev%20-condebug%20-game%20${icefolder}%20-windowed%20-gl%20-w%20640%20-h%20480%20+developer%202%20+log%20on%20+sv_lan%201%20+map%20stalkyard%20+deathmatch%201%20+maxplayers%20${maxplayers}+_sethdmodels%20${hiDef}
+/usr/games/steam steam://rungameid/70//-console%20-dev%20-condebug%20-game%20${icefolder}%20-windowed%20-gl%20-w%20640%20-h%20480%20+developer%202%20+log%20on%20+sv_lan%201%20+map%20snowyard%20+deathmatch%201%20+maxplayers%20${maxplayers}%20+_sethdmodels%20${hiDef}
