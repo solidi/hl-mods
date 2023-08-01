9abf58e3a13f236f905982a768e86364fe4f3b54
Doug Arcuri <xsurrealx@hotmail.com>
2023-07-31 20:24:53 -0400

    Implement slow bullets

--- workspace/Build-Sprites.ps1
+++ workspace/Build-Sprites.ps1
@@ -111,6 +111,7 @@ try {
     Compile-Sprite $binDir "flamepart13" $spritesDir $redistdir\sprites
     Compile-Sprite $binDir "nuke_flicker" $spritesDir $redistdir\sprites
     Compile-Sprite $binDir "640hudrunes" $spritesDir $redistdir\sprites
+    Compile-Sprite $binDir "gameicons2" $spritesDir $redistdir\sprites
     Copy-Item $spritesDir\weapon_vest.txt $redistDir\sprites
     Copy-Item $spritesDir\weapon_knife.txt $redistDir\sprites
     Copy-Item $spritesDir\weapon_9mmhandgun.txt $redistDir\sprites
